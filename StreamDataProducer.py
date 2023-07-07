e84984ef33a4e6bdd82864e160df6e14d3c93a33
MUmarAmanat <MUmarAmanat>
2023-07-07 07:47:33 +0000

    Data producer completed

--- StreamDataProducer.py
+++ StreamDataProducer.py
@@ -1,21 +1,29 @@
 # Databricks notebook source
 """
 Author: Muhammad Umar Amanat
-Description: Notebook for creating Kinesis string using python boto3 api
-Requriements: No 
+Description: Notebook for creating Kinesis stream using python boto3 api
+Requriements: Data should be present at dbfs:/FileStore/dlt_aws 
 Version: V1
 """
 
 # COMMAND ----------
 
 import boto3
+import random as rnd
+import json
+
+from pyspark.sql import functions as F
 
 # COMMAND ----------
 
-kinesis_client = boto3.client('kinesis', 
-                              aws_access_key_id=dbutils.secrets.get("DemoScope1", "aws_access_key_id"), 
-                              aws_secret_access_key=dbutils.secrets.get("DemoScope1", "aws_secret_access_key"),
-                              region_name="us-west-2")
+SCOPE_NAME = "dlt_aws_scope"
+CSV_PATH = "dbfs:/FileStore/dlt_aws"
+KINESIS_REGION = "us-west-2"
+KINESIS_DATA_STREAM = "data-stream"
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
@@ -24,17 +32,58 @@ kinesis_client = boto3.client('kinesis',
 
 # COMMAND ----------
 
-stream_list = ["ecom-wish-list", "ecom-stream"]
-# kinesis_client.update_stream_mode(f"dbutils.secrets.get("DemoScope1", "stream_arn")/s_name" )
+kinesis_client = boto3.client('kinesis', 
+                              aws_access_key_id=dbutils.secrets.get(SCOPE_NAME, "aws_access_key_id"), 
+                              aws_secret_access_key=dbutils.secrets.get(SCOPE_NAME, "aws_secret_access_key"),
+                              region_name=KINESIS_REGION)
+
+# COMMAND ----------
+
+stream_list = [KINESIS_DATA_STREAM]
 
 for s_name in stream_list:
   try:
-    kinesis_client.create_stream(StreamName=s_name, StreamModeDetails={'StreamMode':'ON_DEMAND'})
-    
-      
+    kinesis_client.create_stream(StreamName=s_name, StreamModeDetails={'StreamMode':'ON_DEMAND'})  
     print(f"[INFO] {s_name} created successfully")
   except Exception as e:
-    print(f"[ERR] While creating stream={s_name}, following error occured. {e}")
+    print(f"[ERROR] While creating stream={s_name}, following error occured. {e}")
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Data Generator
+
+# COMMAND ----------
+
+cc = rnd.choice(["MX", "RU"]) ## country code
+
+##randomly select 10 records
+df = (spark
+        .read
+        .option("header", True)
+        .option("multiline", True)
+        .csv(path=f"{CSV_PATH}/{cc}videos.csv")
+        .select("video_id", "trending_date", "title", "channel_title", "category_id",
+                "publish_time", "views", "likes", "dislikes", "comment_count")
+        .sample(withReplacement=False, fraction=1.0)
+        .limit(10)
+      ).toPandas()
+
+# COMMAND ----------
+
+for ind, i in df.iterrows():
+  try:
+    response = (kinesis_client
+                  .put_record(StreamName=KINESIS_DATA_STREAM,
+                              Data=json.dumps(i.to_dict()),
+                              PartitionKey="category_id")
+                ) 
+  except Exception as e:
+    print(f"error occured {e}")
 
 # COMMAND ----------
 
